94e3af2fa4c8d6843c565c77c6a3c44cec9a25c2
m.ivanov <m.ivanov@epsilontel.com>
2023-05-17 12:48:22 +0300

    feat: added testSeven.cy.js fix: refactor selectors

---
+++ cypress/e2e/testSeven.cy.js
@@ -0,0 +1,20 @@
+import globalElements from '../pages/globalElements'
+import homePage from '../pages/homePage'
+//Not sure if there is another way to test this case without external plugins
+describe('7. Check if "get the app" and "watch video" buttons are displayed one above another', () => {
+    it('switches the resolution to iphone-x and checks the order of the buttons', () => {
+        cy.visit(globalElements.homePageUrl)
+        cy.viewport('iphone-x')
+        cy.get('.framer-15y7aei')
+            .children().each(($btn, index) => {
+                if (index === 0){
+                    cy.wrap($btn)
+                        .should('contain.text', homePage.elements.getTheAppButtonText())
+                }
+                else if (index === 1){
+                    cy.wrap($btn)
+                        .should('contain.text', homePage.elements.watchVideoText())
+                }
+        })
+    })
+})
